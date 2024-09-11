d59228659afdfe99fa6677050fc5ff1f23369b7b
shakhob11 <shakhob.work@gmail.com>
2024-09-11 12:41:03 +0500

    Updated!

--- lib/pages/add/pages/add_page.dart
+++ lib/pages/add/pages/add_page.dart
@@ -1,10 +1,12 @@
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kelishamiz/core/extension/context_extension.dart';
 import 'package:kelishamiz/core/extension/widget_extension.dart';
+import 'package:kelishamiz/core/widgets/app_button.dart';
 import 'package:kelishamiz/core/widgets/drop_down_with_title.dart';
 import 'package:kelishamiz/core/widgets/text_field_with_title.dart';
+import 'package:kelishamiz/pages/add/pages/widgets/add_image.dart';
+import 'package:kelishamiz/pages/add/pages/widgets/price_type_button.dart';
 import 'package:provider/provider.dart';
 
 import '../data/models/add_ui_model.dart';
@@ -117,16 +119,47 @@ class _AddPageState extends State<AddPage> {
                   title: "Tavsif(Tavsigfa to’liqroq ma’lumot yozing)",
                   maxLines: 5,
                 ),
-                Switch.adaptive(
-                  activeColor: context.colorScheme.primary,
-                  value: true,
-                  thumbIcon: WidgetStateProperty.all(Icon(
-                    Icons.pause,
-                    color: context.color.lightGrey,
-                  )),
-                  onChanged: (value) {
+                Row(
+                  children: [
+                    Text(
+                      'Narxni kelishish',
+                      style: context.textTheme.bodyLarge,
+                    ),
+                    const Spacer(),
+                    Switch(
+                      activeColor: context.colorScheme.primary,
+                      value: true,
+                      thumbIcon: WidgetStateProperty.all(Icon(
+                        Icons.pause,
+                        color: context.color.lightGrey,
+                      )),
+                      onChanged: (value) {
 
-                },)
+                      },),
+                  ],
+                ),
+                PriceTypeButton(
+                  onPressed: (index) {
+                    print(index);
+                  },
+                ),
+
+                const TextFieldWithTitle(
+                  title: 'Qiymati',
+                  hintText: 'Qiymatini kiriting',
+                ),
+
+                AddImage(),
+
+                AppButton(
+                  onPressed: (){},
+                  text: "E'lon yuklash",
+                  height: 50,
+                  fillTextStyle: context.textTheme.titleMedium!.copyWith(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                ),
               ].paddingEach(const EdgeInsets.symmetric(horizontal: 15, vertical: 10)),
             ),
           ],
@@ -134,4 +167,44 @@ class _AddPageState extends State<AddPage> {
       ),
     );
   }
+}
+
+
+class DashedBorderPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.black
+
+      ..strokeWidth = 2.0
+      ..style = PaintingStyle.stroke;
+
+    final dashLength = 10.0; // Adjust the dash length as needed
+    final gapLength = 5.0; // Adjust the gap length as needed
+
+    final path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+
+    canvas.drawPath(path, paint);
+
+
+    // Draw the dashed lines
+    final dashPath = Path();
+    for (double x = 0; x < size.width; x += dashLength + gapLength) {
+    dashPath.moveTo(x, 0);
+    dashPath.lineTo(x + dashLength, 0);
+    dashPath.moveTo(x, size.height);
+    dashPath.lineTo(x + dashLength, size.height);
+    }
+
+    canvas.drawPath(dashPath, paint);
+    }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
---
+++ lib/pages/add/pages/widgets/add_image.dart
@@ -0,0 +1,96 @@
+
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+import 'package:kelishamiz/core/extension/context_extension.dart';
+
+class AddImage extends StatelessWidget {
+  const AddImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 200,
+          width: double.infinity,
+          child: DottedBorder(
+              color: context.color.grey,
+              radius: const Radius.circular(10),
+              borderType: BorderType.RRect,
+              dashPattern: const [5],
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.image_outlined, color: context.color.grey,),
+                    Text('Rasm yuklash', style: context.textTheme.titleMedium!.copyWith(color: context.color.grey),),
+                  ],
+                ),
+              )
+          ),
+        ),
+        const SizedBox(height: 10,),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 75,
+                    width: 75,
+                    padding: const EdgeInsets.all(5),
+                    child: DottedBorder(
+                      color: context.color.grey,
+                      radius: const Radius.circular(10),
+                      borderType: BorderType.RRect,
+                      dashPattern: const [5],
+                      child: const Center(child: Text('')),
+                    ),
+                  ),
+                  Container(
+                    height: 75,
+                    width: 75,
+                    padding: const EdgeInsets.all(5),
+                    child: DottedBorder(
+                      color: context.color.grey,
+                      radius: const Radius.circular(10),
+                      borderType: BorderType.RRect,
+                      dashPattern: const [5],
+                      child: const Center(child: Text('')),
+                    ),
+                  ),
+                  Container(
+                    height: 75,
+                    width: 75,
+                    padding: const EdgeInsets.all(5),
+                    child: DottedBorder(
+                      color: context.color.grey,
+                      radius: const Radius.circular(10),
+                      borderType: BorderType.RRect,
+                      dashPattern: const [5],
+                      child: const Center(child: Text('')),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: 75,
+                width: 75,
+                padding: const EdgeInsets.all(5),
+                child: DottedBorder(
+                  color: context.color.grey,
+                  radius: const Radius.circular(10),
+                  borderType: BorderType.RRect,
+                  dashPattern: const [5],
+                  child: const Center(child: Icon(Icons.add))
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
