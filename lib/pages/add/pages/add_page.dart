47ce83b38b6e3401d6f28af7fb732c6183f350ed
shakhob11 <shakhob.work@gmail.com>
2024-09-12 23:17:58 +0500

    Updated!

--- lib/pages/add/pages/add_page.dart
+++ lib/pages/add/pages/add_page.dart
@@ -9,6 +9,7 @@ import 'package:kelishamiz/core/extension/context_extension.dart';
 import 'package:kelishamiz/core/extension/widget_extension.dart';
 import 'package:kelishamiz/core/widgets/app_button.dart';
 import 'package:kelishamiz/core/widgets/drop_down_with_title.dart';
+import 'package:kelishamiz/core/widgets/region_select_menu.dart';
 import 'package:kelishamiz/core/widgets/text_field_with_title.dart';
 import 'package:kelishamiz/pages/add/pages/widgets/add_image.dart';
 import 'package:kelishamiz/pages/add/pages/widgets/price_type_button.dart';
@@ -24,238 +25,7 @@ class AddPage extends StatefulWidget {
   State<AddPage> createState() => _AddPageState();
 }
 
-
-class RegionModel {
-  String name = '';
-  List<String> provinces = [];
-  RegionModel({required this.name, required this.provinces});
-}
-
 class _AddPageState extends State<AddPage> {
-
-  MenuController menuController = MenuController();
-  String menuValue = 'Mening';
-
-  List<RegionModel> regions = [
-    RegionModel(
-      name: 'Andijon',
-      provinces: [
-        "Andijon tumani",
-        "Andijon shahar",
-        "Asaka tumani",
-        "Baliqchi tumani",
-        "Bo'z tumani",
-        "Izboskan tumani",
-        "Jalaquduq tumani",
-        "Marhamat tumani",
-        "Oltin tumani",
-        "Shahrihon tumani",
-        "Xo'jaobod tumani",
-        "Xonabad tumani",
-      ],
-    ),
-
-    RegionModel(
-      name: 'Buxoro',
-      provinces: [
-        "Buxoro tumani",
-        "Buxoro shahar",
-        "Gijduvon tumani",
-        "Jondor tumani",
-        "Kogon tumani",
-        "Qorako‘l tumani",
-        "Romitan tumani",
-        "Shofirkon tumani",
-        "Vobkent tumani",
-        "Peshku tumani",
-      ],
-    ),
-    //
-    RegionModel(
-      name: "Farg'ona",
-      provinces: [
-        "Farg'ona tumani",
-        "Farg'ona shahar",
-        "Bag'dod tumani",
-        "Beshariq tumani",
-        "Dang'ara tumani",
-        "Farg'ona tumani",
-        "Qo'qon shahar",
-        "Quva tumani",
-        "Rishton tumani",
-        "Oltiariq tumani",
-        "Uchko'prik tumani",
-
-      ],
-    ),
-    //
-    RegionModel(
-      name: 'Jizzax',
-      provinces: [
-        "Jizzax tumani",
-        "Jizzax shahar",
-        "Arnasoy tumani",
-        "Baxmal tumani",
-        "Do'stlik tumani",
-        "Gallaorol tumani",
-        "Jizzax tumani",
-        "Mirzachol tumani",
-        "Forish tumani",
-        "Yangiobod tumani",
-
-      ],
-    ),
-    //
-    RegionModel(
-      name: 'Namangan',
-      provinces: [
-        "Namangan shahar",
-        "Chortoq tumani",
-        "Chust tumani",
-        "Kosonsoy tumani",
-        "Namangan tumani",
-        "Norin tumani",
-        "Pop tumani",
-        "To'raqo'rg'on tumani",
-        "Uychi tumani",
-      ],
-    ),
-    //
-    RegionModel(
-      name: 'Navoiy',
-      provinces: [
-        "Navoiy tumani",
-        "Navoiy shahar",
-        "Karmana tumani",
-        "Navbahor tumani",
-        "Qiziltepa tumani",
-        "Tomdi tumani",
-        "Uchkuduk tumani",
-        "Xatirchi tumani",
-        "Yangirob tumani",
-
-      ],
-    ),
-    //
-    RegionModel(
-      name: 'Qashqadaryo',
-      provinces: [
-        "Qashqadaryo tumani",
-        "Qashqadaryo shahar",
-        "Bag'dod tumani",
-        "Chiroqchi tumani",
-        "Dehqonobod tumani",
-        "G'uzor tumani",
-        "Kitob tumani",
-        "Mirishkor tumani",
-        "Muborak tumani",
-        "Nishon tumani",
-        "Shahrisabz tumani",
-
-      ],
-    ),
-    //
-    RegionModel(
-      name: 'Samarqand',
-      provinces: [
-        "Samarqand tumani",
-        "Samarqand shahar",
-        "Bulung'ur tumani",
-        "Jomboy tumani",
-        "Kattakurgan tumani",
-        "Ishtikhan tumani",
-        "Oqdaryo tumani",
-        "Payariq tumani",
-        "Pastdarg'om tumani",
-        "Samarkand tumani",
-        "Urgut tumani",
-
-      ],
-    ),
-    //
-    RegionModel(
-      name: 'Sirdaryo',
-      provinces: [
-        "Sirdaryo tumani",
-        "Sirdaryo shahar",
-        "Gulistan tumani",
-        "Boyovut tumani",
-        "Sayxunobod tumani",
-        "Mirzaobod tumani",
-        "Yangiyer tumani",
-
-      ],
-    ),
-    //
-    RegionModel(
-      name: 'Surxondaryo',
-      provinces: [
-        "Surxondaryo tumani",
-        "Termiz shahar",
-        "Angor tumani",
-        "Denov tumani",
-        "Boysun tumani",
-        "Jarqo'rg'on tumani",
-        "Muzrobod tumani",
-        "Oltinsoy tumani",
-        "Sariosiyo tumani",
-        "Shurchi tumani",
-        "Surxondaryo tumani",
-
-      ],
-    ),
-    //
-    RegionModel(
-      name: 'Toshkent',
-      provinces: [
-        "Toshkent tumani",
-        "Toshkent shahar",
-        "Angren tumani",
-        "Bekabad tumani",
-        "Bo'zsuv tumani",
-        "Chirchiq tumani",
-        "O'rtachirchiq tumani",
-        "Pskent tumani",
-        "Tashkent tumani",
-        "Ohangaron tumani",
-
-      ],
-    ),
-    //
-    RegionModel(
-      name: 'Xorazm',
-      provinces: [
-        "Xorazm tumani",
-        "Urganch shahar",
-        "Bog'ot tumani",
-        "Gurlan tumani",
-        "Xiva tumani",
-        "Shovot tumani",
-        "Yangibozor tumani",
-        "Xazorasp"
-      ],
-    ),
-    //
-    RegionModel(
-      name: "Qoraqalpog'iston",
-      provinces: [
-        "Amudaryo tumani",
-        "Beruniy tumani",
-        "Beyneu tumani",
-        "Ellikqal'a tumani",
-        "Jumurt tumani",
-        "Kogon tumani",
-        "Muzrabot tumani",
-        "Nukus tumani",
-        "Shumanay tumani",
-        "Taxtakupir tumani",
-        "Turtkul tumani",
-        "Uchkuduk tumani",
-        "Xo'jayli tumani",
-      ],
-    ),
-
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -275,10 +45,10 @@ class _AddPageState extends State<AddPage> {
                     watch.selectedAddCategoryUIModel.title,
                     style: context.textTheme.titleLarge,
                   ),
-                  TextFieldWithTitle(
+
+                  const TextFieldWithTitle(
                     title: "Ism to'liq",
                   ),
-
                   const TextFieldWithTitle(
                     title: "E-mail",
                   ),
@@ -289,41 +59,13 @@ class _AddPageState extends State<AddPage> {
                     keyboardType: TextInputType.phone,
                   ),
 
-
-
-
-                  MenuBar(
-                    style: const MenuStyle(
-                      elevation: WidgetStatePropertyAll(10),
-                      padding: WidgetStatePropertyAll(EdgeInsets.zero),
-                      fixedSize: null,
-                      maximumSize: null,
-                    ),
-                    children: [
-                      SubmenuButton(
-                        controller: menuController,
-                        menuChildren: regions.map((region) {
-                          return SubmenuButton(
-                            menuChildren: region.provinces.map((e) {
-                              return TextButton(
-                                onPressed: (){
-                                  print(region.name);
-                                  print(e);
-                                  menuController.close();
-                                },
-                                child: Text(e),
-                              );
-                            }).toList(),
-                            child: Text(region.name),
-                          );
-                        }).toList(),
-                        child: Text("ffffff"),
-                      ),
-                    ],
+                  RegionSelectMenu(
+                    regionMenuValueType: RegionMenuValueType.full,
+                    menuType: MenuType.add,
+                    onChange: (value) {
+                      print(value);
+                    },
                   ),
-
-
-
 
                   DropDownWithTitle(
                     onChanged: (value) {
